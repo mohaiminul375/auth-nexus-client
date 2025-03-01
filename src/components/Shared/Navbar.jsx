3730d2f7734638c95181c1e187815d8db2b2b472
Mohaiminul375 <mohaiminul375@gmail.com>
2025-03-01 22:45:14 +0600

    change config shadcn

--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -1,6 +1,6 @@
 // hsl(246.74,48.11%,36.27%)
 const Navbar = () => {
-  return <div className="h-32 bg-[#3A3089] flex justify-center items-center">
+  return <div className="h-32 bg-popover-foreground flex justify-center items-center">
     <button className="p-4 rounded-full bg-[#f8669e] text-white">Demo button</button>
   </div>;
 };
